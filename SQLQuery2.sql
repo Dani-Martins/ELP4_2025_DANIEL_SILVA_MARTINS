9dbf56f9daa77acda61724a2c059ce40ded028bc
Dani-Martins <silvamartinsdaniel235@gmail.com>
2025-10-19 23:55:37 -0300

    Sexto Commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿create table estados(
+	id int not null identity(10, 1) primary key,
+	estado varchar (55) not null,
+	uf varchar (3),
+	oPais varchar (55) not null
+)
---
+++ SQLQuery3.sql
@@ -0,0 +1,6 @@
+﻿create table cidades(
+	id int not null identity(10, 1) primary key,
+	cidade varchar (55) not null,
+	ddd varchar (4),
+	oEstado varchar (55) not null
+)
